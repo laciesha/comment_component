cf0d2589ef465a4cf90b94992ce1e17996436bcc
laciesha <laciesha_venus@yahoo.com>
2022-08-27 19:11:28 -0500

    offline

--- workbox-lab/project/workbox-config.js
+++ workbox-lab/project/workbox-config.js
@@ -1,8 +1,17 @@
 module.exports = {
   "globDirectory": "build/",
   "globPatterns": [
-    "**/*.{jpg,svg,html,js,css}"
+    "**/*.css",
+    "index.html",
+    "js/animation.js",
+    "images/home/*.jpg",
+    "images/icon/*.svg",
+    "pages/offline.html",
+    "pages/404.html"
   ],
+  "swSrc": "src/sw.js",
   "swDest": "build/sw.js",
-  "swSrc": "src/sw.js"
+  "globIgnores": [
+    "../workbox-config.js"
+  ]
 };
