568e65eb958b567a372a8704a1f70b1ea8390cac
laciesha <laciesha_venus@yahoo.com>
2022-09-05 12:00:42 -0500

    updates2

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "",
+  build: {
+    outDir: "dist",
+    base: ""
+  },
+
+  //   plugins: [
+  //     VitePWA({
+  //       injectManifest: true,
+  //     }),
+  //   ],
+});
